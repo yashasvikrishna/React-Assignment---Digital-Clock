0ab9e880b3c5c7799c393ca847428bf662a36eef
Siddharth Saxena <siddy5691@gmail.com>
2020-12-09 20:09:33 +0530

    created clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+// class App extends Component {
+//     constructor(){
+//         let time = new Date().toLocaleTimeString();
+//         this.state = {
+//             ctime: time
+//         }
+//     }
 
-        return(
-            <>
-               
-            </>
-        )
+//     const updateTime
+
+//     render() {
+
+//         // let time = new Date().toLocaleTimeString();
+//         return(
+//             <>
+//                 <h1>{ctime}</h1>
+//             </>
+//         )
+//     }
+// }
+
+const App = () => {
+    let time = new Date().toLocaleTimeString();
+
+    const [ctime, setCtime] = useState(time);
+
+    const updateTime = () => {
+        let time = new Date().toLocaleTimeString();
+        setCtime(time);
     }
+
+    setInterval(updateTime, 1000);
+
+    return(
+        <>
+            <h1 className="Clock">{ctime}</h1>
+        </>
+    )
 }
 
 
