5647588ed8e1f04590dfabb39080a4986b8e6236
Siddharth Saxena <siddy5691@gmail.com>
2020-12-10 19:17:39 +0530

    answer

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,54 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(){
-        super()
+    constructor() {
+        super();
         this.state = {
-            time: new Date()
-        }
+            time: new Date(),
+        };
+        this.intervalId = null;
     }
 
-    updateTime(){
-        this.setState({
-            time: new Date()
-        })
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState({
+                time: new Date()
+            })
+        }, 1*1000);
     }
 
-    componentWillMount(){
-        this.myInterval = setInterval(() => this.updateTime(), 1000)
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.myInterval);
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
     }
 
     render() {
-        return(
-            <>
-                <div className="Clock">
-                    <h1>{this.state.time.toLocaleTimeString()}</h1>
-                </div>
-            </>
-        )
+        return (
+            <div className="Clock">
+                <h3 id="time">{this.getTimeString()}</h3>
+            </div>
+        );
+    }
+
+    getTimeString() {
+        const currTime = this.state.time;
+        const [hours, minutes, seconds] = [
+            currTime.getHours(),
+            currTime.getMinutes(),
+            currTime.getSeconds(),
+        ];
+
+        const amOrpm = hours >= 12 ? "PM" : "AM";
+        const twelveHourFormat = hours > 12 ? hours - 12 : hours;
+        const hourString = this.padNumberToTwoDigits(twelveHourFormat);
+        const minuteString = this.padNumberToTwoDigits(minutes);
+        const secondString = this.padNumberToTwoDigits(seconds);
+
+        const timeString =  `${hourString}:${minuteString}:${secondString} ${amOrpm}`;
+        return timeString;
+    }
+
+    padNumberToTwoDigits(num) {
+        return `${num < 10 ? "0": ""}${num}`
     }
 }
 
