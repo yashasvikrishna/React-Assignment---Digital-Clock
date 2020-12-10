cd3b260dd4f9b850ea9ddf29954f4f4151a53247
Siddharth Saxena <siddy5691@gmail.com>
2020-12-10 09:26:31 +0530

    add class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,37 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-// class App extends Component {
-//     constructor(){
-//         let time = new Date().toLocaleTimeString();
-//         this.state = {
-//             ctime: time
-//         }
-//     }
-
-//     const updateTime
-
-//     render() {
-
-//         // let time = new Date().toLocaleTimeString();
-//         return(
-//             <>
-//                 <h1>{ctime}</h1>
-//             </>
-//         )
-//     }
-// }
-
-const App = () => {
-    let time = new Date().toLocaleTimeString();
-
-    const [ctime, setCtime] = useState(time);
-
-    const updateTime = () => {
-        let time = new Date().toLocaleTimeString();
-        setCtime(time);
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            time: new Date()
+        }
     }
 
-    setInterval(updateTime, 1000);
+    updateTime(){
+        this.setState({
+            time: new Date()
+        })
+    }
 
-    return(
-        <>
-            <h1 className="Clock">{ctime}</h1>
-        </>
-    )
+    componentWillMount(){
+        this.myInterval = setInterval(() => this.updateTime(), 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.myInterval);
+    }
+
+    render() {
+        return(
+            <>
+                <div className="Clock">
+                    <h1>{this.state.time.toLocaleTimeString()}</h1>
+                </div>
+            </>
+        )
+    }
 }
-
 
 export default App;
